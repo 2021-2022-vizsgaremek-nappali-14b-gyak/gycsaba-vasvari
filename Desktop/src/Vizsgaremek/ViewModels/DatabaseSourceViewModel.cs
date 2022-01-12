f88916b79096af2100219c693552432583079fb5
Csaba Gyuris <gycsaba@vasvari.hu>
2022-01-12 14:29:44 +0100

    ViewModel és View

---
+++ Desktop/src/Vizsgaremek/ViewModels/DatabaseSourceViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections.ObjectModel;
+using Vizsgaremek.Repositories;
+
+namespace Vizsgaremek.ViewModels
+{
+    class DatabaseSourceViewModel
+    {
+        private ObservableCollection<string> displayedDatabaseSource;
+        private string selectedDatabaseSource;
+        DatabaseSouerces repoDatabaseSouerces;
+
+
+        public ObservableCollection<string> DisplayedDatabaseSource 
+        { 
+            get => displayedDatabaseSource; 
+        }
+        public string SelectedDatabaseSource 
+        { 
+            get => selectedDatabaseSource; 
+            set => selectedDatabaseSource = value; 
+        }
+
+        public DatabaseSourceViewModel()
+        {
+            repoDatabaseSouerces = new DatabaseSouerces();
+            displayedDatabaseSource = new ObservableCollection<string>(repoDatabaseSouerces.GetAllDatabaseSources());
+
+        }
+    }
+}
--- Desktop/src/Vizsgaremek/Views/Pages/DatabaseSourcePage.xaml.cs
+++ Desktop/src/Vizsgaremek/Views/Pages/DatabaseSourcePage.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 using Vizsgaremek.Views.Navigation;
+using Vizsgaremek.ViewModels;
 
 namespace Vizsgaremek.Views.Pages
 {
@@ -22,9 +23,13 @@ namespace Vizsgaremek.Views.Pages
     /// </summary>
     public partial class DatabaseSourcePage : UserControl
     {
+        DatabaseSourceViewModel databaseSourceViewModel;
+
         public DatabaseSourcePage()
         {
+            databaseSourceViewModel = new DatabaseSourceViewModel();
             InitializeComponent();
+            this.DataContext = databaseSourceViewModel;
         }
 
         private void Image_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
