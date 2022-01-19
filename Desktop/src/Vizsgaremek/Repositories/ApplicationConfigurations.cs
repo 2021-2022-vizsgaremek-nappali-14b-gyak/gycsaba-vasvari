8100c3843fdc0ffba265c4291dc14075533a9e87
Csaba Gyuris <gycsaba@vasvari.hu>
2022-01-19 13:26:23 +0100

    App.config fájlba egy adat és repoba ApplicationConfigurations egy adatot kiolvasás

---
+++ Desktop/src/Vizsgaremek/Repositories/ApplicationConfigurations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Configuration;
+
+namespace Vizsgaremek.Repositories
+{
+    class ApplicationConfigurations
+    {
+        private string name;
+
+        public string Name { get => name; set => name = value; }
+
+        public ApplicationConfigurations()
+        {
+            var name = ConfigurationManager.AppSettings.Get("name");
+        }
+    }
+}
