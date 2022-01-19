a04f92659d78314a4bf670fb553c2b57e5f0a0c6
Csaba Gyuris <gycsaba@vasvari.hu>
2022-01-19 12:59:34 +0100

    Többnyelvű és verziófüggő menü

--- Desktop/src/Vizsgaremek/Properties/Settings.Designer.cs
+++ Desktop/src/Vizsgaremek/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace Vizsgaremek.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool developVersion {
             get {
                 return ((bool)(this["developVersion"]));
