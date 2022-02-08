d3cdb9f331e7c9a1bd35ffdd4ce4ca06bc1f98db
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-02-08 12:20:50 +0100

    EditedTeacher property a TeacherControlViewModel-ben

--- Desktop/src/Vizsgaremek/ViewModels/TeacherControlViewModel.cs
+++ Desktop/src/Vizsgaremek/ViewModels/TeacherControlViewModel.cs
@@ -4,9 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Vizsgaremek.Models;
+
 namespace Vizsgaremek.ViewModels
 {
     class TeacherControlViewModel
     {
+        private Teacher editedTeacher;
+
+        public Teacher EditedTeacher
+        {
+            get
+            {
+                return editedTeacher;
+            }
+            set
+            {
+                editedTeacher = value;
+            }
+        }
+
+        public TeacherControlViewModel()
+        {
+            editedTeacher = new Teacher();
+        }
     }
 }
