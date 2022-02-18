ad3c17e73297f440aa5b0d1fc5d480d62c14133e
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-02-18 08:42:45 +0100

    Az osztálynak ismernie kell a validálandó stringet, a nyelvi szótárt, hogy az adott nyelven jelenítse meg a hibaüzenetet és visszad egy hibaüzenetet ha hiba van

--- Desktop/src/Vizsgaremek/Validations/ValidationRules/OnNameValidation.cs
+++ Desktop/src/Vizsgaremek/Validations/ValidationRules/OnNameValidation.cs
@@ -4,9 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Windows;
+
 namespace Vizsgaremek.Validations.ValidationRules
 {
     class OnNameValidation
     {
+        private string stringToValidate;
+        private string errorMessage;
+        private ResourceDictionary dictionary;
     }
 }
