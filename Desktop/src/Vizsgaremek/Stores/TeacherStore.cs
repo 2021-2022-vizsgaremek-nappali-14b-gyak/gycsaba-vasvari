50775534139e7d70770daf786201f9752d3c7f15
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-02-28 10:13:09 +0100

    A tanár törlésekör, módosításakor vagy új tanár módosításakor a TeacherControl és TeacherPage kommunikációjára van szükség. Erre használjuk a TeacherStrore-t

---
+++ Desktop/src/Vizsgaremek/Stores/TeacherStore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vizsgaremek.Stores
+{
+    public class TeacherStore
+    {
+        // https://www.youtube.com/watch?v=o62iFhXkWS4
+        // https://github.com/SingletonSean/wpf-tutorials/tree/master/CommunicationMVVM
+    }
+}
