f965327176471d969589d89dff15fd9aedf4ed4d
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-02-28 10:16:44 +0100

    Implementálva az interfacet kapunk egy eseményt és két metódust

--- Desktop/src/Vizsgaremek/Command/CommandBase.cs
+++ Desktop/src/Vizsgaremek/Command/CommandBase.cs
@@ -11,5 +11,16 @@ namespace Vizsgaremek.Commands
     // https://www.c-sharpcorner.com/UploadFile/e06010/wpf-icommand-in-mvvm/
     public abstract class CommandBase : ICommand
     {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
