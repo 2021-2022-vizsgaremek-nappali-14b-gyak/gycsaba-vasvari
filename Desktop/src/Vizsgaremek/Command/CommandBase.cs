b9d0f7f302a426aa63d2c1487e7ba8bcd7641a12
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-02-28 10:16:12 +0100

    Ezeket a parancsokat az ICommand interfacből szármatatjuk.

---
+++ Desktop/src/Vizsgaremek/Command/CommandBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Input;
+
+namespace Vizsgaremek.Commands
+{
+    // https://www.c-sharpcorner.com/UploadFile/e06010/wpf-icommand-in-mvvm/
+    public abstract class CommandBase : ICommand
+    {
+    }
+}
