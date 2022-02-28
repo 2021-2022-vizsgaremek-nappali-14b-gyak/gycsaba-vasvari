a2fb358dfa82f475ccc9fb0613e037dd02234fa8
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-02-28 10:29:54 +0100

    A DeleteTeacherCommand adattagjai a kommunikációhoz

--- Desktop/src/Vizsgaremek/Command/DeleteTeacherCommand.cs
+++ Desktop/src/Vizsgaremek/Command/DeleteTeacherCommand.cs
@@ -4,9 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Vizsgaremek.ViewModels;
+using Vizsgaremek.Stores;
+
 namespace Vizsgaremek.Command
 {
     class DeleteTeacherCommand
     {
+        // Ez a ViewModel tudja melyik tanárt kell törölni
+        private readonly TeacherControlViewModel teacherControlViewModel;
+        // Ebben a Store-ban valósítjuk meg a törlést
+        private readonly TeacherStore teacherStore;
     }
 }
