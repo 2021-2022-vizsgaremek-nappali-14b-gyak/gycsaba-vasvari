8acb1d2b6e719332e0a34cb88f6bdb6ff8a238df
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-03-10 11:31:33 +0100

    A módosítást a teszt adatokra végezzük el.

--- Desktop/src/Vizsgaremek/Repositories/TeachersAPI.cs
+++ Desktop/src/Vizsgaremek/Repositories/TeachersAPI.cs
@@ -63,6 +63,7 @@ namespace Vizsgaremek.Repositories
             switch (applicationStore.DbSource)
             {
                 case DbSource.NONE:
+                    UpdateTeacherInTestData(id, entity);
                     break;
                 case DbSource.LOCALHOST:
                     break;
