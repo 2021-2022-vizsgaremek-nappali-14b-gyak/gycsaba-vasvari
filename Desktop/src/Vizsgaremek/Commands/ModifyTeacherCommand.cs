53150160a507f7046c94bb7cfe8407a728d1c1ef
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-03-10 10:31:55 +0100

    Ekészítjük a ModifyTeacherCommand parancsot amely a CommandBase osztályból származik

---
+++ Desktop/src/Vizsgaremek/Commands/ModifyTeacherCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vizsgaremek.Commands
+{
+    class ModifyTeacherCommand : CommandBase
+    {
+        public override bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public override void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
