e6668760fc974eb8ba962b290fd4a5f696a7aa0f
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-03-10 10:33:20 +0100

    A parancsa a TeacherControlViewModelből kapja a módosított tanár adatait, és a TeacherStore-t felhasználva végzi el a módosítást

--- Desktop/src/Vizsgaremek/Commands/ModifyTeacherCommand.cs
+++ Desktop/src/Vizsgaremek/Commands/ModifyTeacherCommand.cs
@@ -4,10 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Vizsgaremek.ViewModels;
+using Vizsgaremek.Stores;
+
 namespace Vizsgaremek.Commands
 {
     class ModifyTeacherCommand : CommandBase
     {
+        private readonly TeacherControlViewModel teacherControlViewModel;
+        private readonly TeacherStore teacherStore;
         public override bool CanExecute(object parameter)
         {
             return true;
