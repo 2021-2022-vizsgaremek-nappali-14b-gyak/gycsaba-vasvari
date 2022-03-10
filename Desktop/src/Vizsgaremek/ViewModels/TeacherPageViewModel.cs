324dfb10fd3f22ee5635298cda85961dbbb9317d
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-03-10 11:40:15 +0100

    Pontosítjuk a metódus nevét az API-nak megfelelően

--- Desktop/src/Vizsgaremek/ViewModels/TeacherPageViewModel.cs
+++ Desktop/src/Vizsgaremek/ViewModels/TeacherPageViewModel.cs
@@ -66,7 +66,7 @@ namespace Vizsgaremek.ViewModels
             string teacherId = modifydTeacher.Id;
             if (teachersRepo.IsTeacherExsist(teacherId))
             {
-                teachersRepo.UpdateTeacher(teacherId, modifydTeacher);
+                teachersRepo.Update(teacherId, modifydTeacher);
             }
         }
 
